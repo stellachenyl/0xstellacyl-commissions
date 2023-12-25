30d7d4280728bb8967d186b8a316cc80411e6c95
Stella <stellachen273@gmail.com>
2023-12-26 04:01:30 +0800

    Initial commit

---
+++ settings.tsx
@@ -0,0 +1,11 @@
+// Purpose: Contains texts that will be used in the UI when rendering the buttons
+// label has to be the exact same as in your candy machine config
+export const mintText = [
+  { label: "default", mintText: "Mint an NFT!", buttonLabel: "Mint now!", header: "Public Mint"}
+];
+
+//header image in the ui. replace with your own
+export const image = "https://cdn.discordapp.com/attachments/1187546925252235398/1188030339814268928/49.png";
+
+//website title
+export const headerText = "stellacyl's commissions";
